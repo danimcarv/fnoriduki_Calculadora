d83f91426c50877004feb2e144449190bc1de4d1
Daniel Monteiro de Carvalho <danimont228@gmail.com>
2024-06-10 12:35:35 -0300

    Resolvido o item 5

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -18,10 +18,13 @@ namespace Calculadora
             Calculadora calculadora = new Calculadora();
 
             
+            Stack<double> resultados = new Stack<double>(); // Implementação da Stack
+
             while (filaOperacoes.Count > 0) // Alterado o parâmetro do while
             { // Após zerar a Queue, ele estava gerando uma exceção devido a tentar remover itens da Queue vazia. Alterar de ">= 0" para "> 0" solucionou o problema
                 Operacoes operacao = filaOperacoes.Dequeue(); // Alterado método. O método Peak retorna o item mas não o remove, o que estava causando o processamento infinito
                 calculadora.calcular(operacao);
+                resultados.Push(operacao.resultado); // Implementação da Stack
                 Console.WriteLine("{0} {1} {2} = {3}", operacao.valorA,operacao.operador,operacao.valorB, operacao.resultado);
                 if (filaOperacoes.Count != 0) Console.WriteLine("Próximas operações:"); // Implementação da lista de operações
                 else Console.WriteLine("Fim das operações"); // Implementação da lista de operações
@@ -32,7 +35,8 @@ namespace Calculadora
                 Console.WriteLine(); // Implementação da lista de operações
             }
 
-          
+            Console.WriteLine("Lista de Resultados (Stack / LIFO)"); // Implementação da Stack
+            foreach (double resultado in resultados) Console.WriteLine(resultado); // Implementação da Stack
            
         }
     }
--- Calculadora/obj/Debug/net5.0/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net5.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d63eaba097a77e2ef35baee35f47255fe6324b5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80b74f0c0dd353370de7b1953f05eeb090650247")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
